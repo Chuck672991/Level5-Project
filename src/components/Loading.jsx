d3980fa663af6e568d056754d3a30f3bca66e2e7
Abdullah <professorshaha99@gmail.com>
2024-03-06 17:38:30 +0500

    update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -25,7 +25,7 @@ const Loading = () => {
   }, []);
 
   return (
-    <div className="loading" style={{background: 'linear-gradient(-140deg, rgba(0,0,0,1) 12%, rgba(0,0,0,1) 61%, rgba(7,4,66,0.9051995798319328) 100%)',height:"97.6vh"}}>
+    <div className="loading" style={{background: 'linear-gradient(143deg, rgba(0,0,0,1) 1%, rgba(7,4,66,1) 47%, rgba(1,1,12,1) 89%)',height:"97.6vh"}}>
       <div className="h-screen">
         <div className={`text-center relative transform scale-75 top-40 z-10 ${is600 ? styles.smallScreen : styles.largeScreen}`}>
           <Image
