c44796eba3b63edbd16d7a52a87ca89556899938
Asim-Zaheer <iasimzaheer.com>
2024-03-06 20:55:23 +0500

    myComp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,23 @@ import Container from './Container';
 import Design from './Design';
 
 export default function Header() {
-  const isSmallScreen = useMediaQuery('min-width:600px)');
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
+  const isLgScreen = useMediaQuery('(min-width:1800px)');
 
   return (
     <>
-      <div style={{  background: 'rgb(28,23,60)', background: 'linear-gradient(-140deg, rgba(0,0,0,1) 12%, rgba(0,0,0,1) 61%, rgba(7,4,66,0.9051995798319328) 100%)' }}>
+      <div style={{  background: 'rgb(28,23,60)', background: 'linear-gradient(-140deg, rgba(0,0,0,1) 12%, rgba(0,0,0,1) 61%, rgba(7,4,66,0.9051995798319328)' }}>
         <Container>
           <Grid container spacing={2}>
             {/* Left Grid Item */}
             <Grid item xs={4} md={4} lg={6} xl={6}>
-              <Box sx={{ paddingLeft: '18%', paddingTop: "20%", width: { xs: "35vh", md: "120vh" } }}>
+              <Box sx={{ paddingLeft: '18%', paddingTop: "20%", width: { xs: "35vh", md: "120vh" }, padding:"23%" }}>
                 <Typography sx={{ fontFamily: "IBM Plex Sans Arabic", color: "#FFFF", fontSize: { xs: "1.5vh", md: "2vh" } }}>
                   IT Systems Company
                 </Typography>
                 <Typography sx={{
                   fontFamily: "",
-                  fontSize: { xs: "2vh", md: "6vh" },
+                  fontSize: { xs: "2vh", md: "4vh",lg:"6vh" ,xl:"8vh"},
                   fontWeight: 600,
                   lineHeight: "",
                   letterSpacing: "0.1em",
@@ -28,7 +29,7 @@ export default function Header() {
                 }}>
                   An IT Boutique House
                 </Typography>
-                <Typography sx={{ borderBottom: "0.4vh solid #8868F9", maxWidth: { xs: "16vh", md: "48vh" }, marginBottom: "5%" }} />
+                <Typography sx={{ borderBottom: "0.4vh solid #8868F9", maxWidth: { xs: "16vh", md: "30vh" }, my: "3%" }} />
                 <Typography sx={{
                   fontFamily: "",
                   fontSize: { xs: "1.5vh", md: "5vh" },
@@ -41,13 +42,13 @@ export default function Header() {
                   Where technology is finely curated & elegantly tailored for every client’s journey
                 </Typography>
               </Box>
-              <Box  >
-                <img src="./thumbnail_Master_logo_white 2 (1).svg" alt="level five logo " style={{ paddingTop:"20%", paddingLeft: isSmallScreen ? "10.5vh" : "3.5vh", width:isSmallScreen?"30vw":"20vw" }} sx={{width:{xs:"20px",md:"50px"}}} />
-              </Box>
+              {/* <Box  >
+                <img src="./thumbnail_Master_logo_white 2 (1).svg" alt="level five logo " style={{ paddingTop:isLgScreen?"18%":"20%",   width:isSmallScreen?"20vw":"25vw" }} sx={{paddingLeft:{xs:"4vh", sm:"2.5vh" , md:"15vh",lg:"26vh",xl:"28vh"}}} />
+              </Box> */}
             </Grid>
             {/* Right Grid Item */}
             <Grid item xs={8} md={8} lg={6} xl={6} >
-              <Box sx={{ paddingLeft: '40%', paddingTop: { xs: "5vh", md: "10vh" }, width: { xs: "28vh", md: "70vh" } }}>
+              <Box sx={{ paddingLeft: {xs:'30%',sm:"58%",md:"60%",lg:"50%",xl:"60%"},paddingTop: { xs: "5vh", md: "4vh" },paddingBottom:{xs:'5vh',md:"30vh"},opacity:{xs:"23%",sm:"23%",md:"50%" , lg:"50%",xl:"50%"}, width: { xs: "28vh", md: "70vh" } }}>
                 <Design />
               </Box>
             </Grid>
