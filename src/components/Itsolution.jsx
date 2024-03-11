f9705d7d8733a69e8eebdf36ad3ca4d7e41eb68a
Abdullah <professorshaha99@gmail.com>
2024-03-11 18:06:41 +0500

    updates story

--- src/components/Itsolution.jsx
+++ src/components/Itsolution.jsx
@@ -77,11 +77,11 @@ export default function Itsolution() {
           </Grid>
           <Grid item xs={12} md={6} lg={6} xl={6}>
             <Box sx={{ paddingRight: { xs: "0", md: "12vh" },  paddingTop: isMobileScreen ? "8%" : "1%", fontWeight: "300",  }}>
-              <Typography sx={{ color: "#C5C5C5", fontFamily: "IBM Plex Sans Arabic", fontSize: {xs:"14px",sm:"16px",md:"15px",lg:"18px"} }}>
+              <Typography sx={{ color: "#C5C5C5", fontFamily: "IBM Plex Sans Arabic", fontSize: {xs:"1vh",sm:"1.5vh",md:"2vh",lg:"2.5vhpx"} }}>
                 We aspire to be the premier IT solutions facilitator in the Kingdom, connecting clients with specialized expertise while maintaining operational agility and cost-effectiveness.
               </Typography>
               <br />
-              <Typography sx={{ color: "#C5C5C5", fontFamily: "IBM Plex Sans Arabic", fontSize: {xs:"14px",sm:"16px",md:"15px",lg:"18px"} }}>
+              <Typography sx={{ color: "#C5C5C5", fontFamily: "IBM Plex Sans Arabic", fontSize: {xs:"0.8",sm:"1vh",md:"1.5vh",lg:"2vhpx"} }}>
                 Headquartered in Riyadh, Kingdom of Saudi Arabia, Level Five is committed to providing innovative, tailored IT solutions by fostering strategic partnerships, leveraging external expertise, and optimizing resource allocation.
                 <br />
                 <br />
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -46,7 +46,7 @@ export default function Story() {
                 <Typography
                   sx={{
                     fontFamily: "IBM Plex Sans Arabic",
-                    fontSize: { xs: "2vh",sm:"1.6vh" ,md: "2.3vh",lg:"3",xl:"3vh" },
+                    fontSize: { xs: "2vh",sm:"1.6vh" ,md: "2.3vh",lg:"4vh",xl:"3.5vh" },
                     fontWeight: "600",
                     lineHeight: {xs:"vh "},
                     letterSpacing: "0em",
@@ -61,7 +61,7 @@ export default function Story() {
                 <Box sx={{ borderBottom: "0.5vh solid #8868F9", my: "2%", maxWidth: { xs: "2.5vw", sm: "3vw", md: "3vw", lg: "4vw" }, }} />
               </Box>
               <Box sx={{ fontFamily:"IBM Plex Sans Arabic",color:"#C5C5C5", paddingLeft: { xs: "2vh", md: "3vh",lg:"10vh",xl:"3vh" } }}>
-                <Typography sx={{ color: "#C5C5C5",fontSize: {xs:"14px",sm:"16px",md:"15px",lg:"1.5vh",xl:"1.5vh"},paddingBottom:"10%" }}>
+                <Typography sx={{ color: "#C5C5C5",fontSize: {xs:"14px",sm:"16px",md:"1.5vh",lg:"2vh",xl:"1.5vh"},paddingBottom:"10%" }}>
                   Level Five Information Systems Technology Company emerged as a boutique IT house with a distinctive story. The choice of “Level Five” was akin to the journey through a boutique, where each level represents a curated space of expertise. Just as one explores unique offering in a boutique, Level Five aspires to deliver specialized and tailored IT solutions at the highest level of proficiency.
 
                   <br /><br />
