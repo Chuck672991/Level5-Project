3af809e321792a605e977a5e30bb719d67db4225
Abdullah <professorshaha99@gmail.com>
2024-03-12 11:56:05 +0500

    responsive layout text

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -60,8 +60,8 @@ export default function Story() {
                 </Typography>
                 <Box sx={{ borderBottom: "0.5vh solid #8868F9", my: "2%", maxWidth: { xs: "20vw", sm: "12vw", md: "15vw", lg: "12vw" }, }} />
               </Box>
-              <Box sx={{ fontFamily:"IBM Plex Sans Arabic",color:"#C5C5C5", paddingLeft: { xs: "2vh", md: "3vh",lg:"3vh",xl:"3vh" } }}>
-                <Typography sx={{ color: "#C5C5C5",fontSize: {xs:"14px",sm:"16px",md:"1.5vh",lg:"2vh",xl:"1.5vh"},paddingBottom:"10%" }}>
+              <Box sx={{ fontFamily:"IBM Plex Sans Arabic",color:"#C5C5C5", paddingLeft: { xs: "2vh", md: "3vh",lg:"2vh",xl:"3vh" } }}>
+                <Typography sx={{ color: "#C5C5C5",fontSize: {xs:"14px",sm:"16px",md:"1.5vh",lg:"1.5vh",xl:"1.5vh"},paddingBottom:"10%" }}>
                   Level Five Information Systems Technology Company emerged as a boutique IT house with a distinctive story. The choice of “Level Five” was akin to the journey through a boutique, where each level represents a curated space of expertise. Just as one explores unique offering in a boutique, Level Five aspires to deliver specialized and tailored IT solutions at the highest level of proficiency.
 
                   <br /><br />
