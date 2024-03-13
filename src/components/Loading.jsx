5c5dd5ef17574043e6ed20ba3ff3e421a37e5829
Danish Ahmed <danish.ahmed90@gmail.com>
2024-03-13 11:19:52 +0500

    updated

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -9,17 +9,21 @@ import styles from "./FilledImage.module.css";
 const Loading = ({ id }) => {
   const is600 = useMediaQuery("(max-width:600px)"); // Check if screen size is 600px or smaller
 
-  
-
   return (
     <div
       id={id}
-    
+
       // className="loading"
     >
       <div>
         <div
-          style={{ background: "black" ,height:"100vh",position:"relative"}}
+          style={{
+            background: "black",
+            height: "100vh",
+            position: "relative",
+            background:
+              "linear-gradient(120deg, rgba(10,4,57,1) 3%, rgba(0,0,0,1) 50%, rgba(11,5,63,1) 99%)",
+          }}
           className={`
        
         
@@ -28,10 +32,10 @@ const Loading = ({ id }) => {
         >
           <Image
             width={is600 ? 300 : 700.6}
-            height={is600 ? 50 : 100.52} 
+            height={is600 ? 50 : 100.52}
             src={FilledLandingLogo}
             alt="Logo"
-            style={{ marginLeft: "10vw" }}
+            style={{ marginLeft: "11vw" }}
             className={styles.logoContainer}
           />
           <Image
@@ -39,12 +43,12 @@ const Loading = ({ id }) => {
             height={is600 ? 50 : 100.52} // Adjusted height based on screen size
             src={EmptyLandingLogo}
             alt="EmptyLandingLogo"
-            style={{position:"absolute",left:'36.6vw',
-            //  translate:'-50% -50%'
-            }
-            }
-
-/>
+            style={{
+              position: "absolute",
+              left: "34.3vw",
+              //  translate:'-50% -50%'
+            }}
+          />
         </div>
       </div>
     </div>
