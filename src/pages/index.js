9b55e0294cf77b42ed86297d8a886e5adb71dcb5
Danish Ahmed <danish.ahmed90@gmail.com>
2024-03-14 22:16:52 +0500

    completed laoding fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,71 +35,139 @@ import Connect from "@/components/Connect";
 import Loading from "@/components/Loading"; // Assuming you have a Loading component
 import gsap, { Expo } from "gsap";
 import Header from "@/components/Header";
+import Navbar from "@/components/Nevbar";
+import { display } from "@mui/system";
 
 const Home = ({ children }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [opacity, setopacity] = useState(0);
 
   useEffect(() => {
-    function loadingFn() {
-      const tl = gsap.timeline();
-      tl.to("#loadingAnimation", {
-        height: 0,
-        duration: 4,
+    // function loadingFn() {
+    //   const tl = gsap.timeline();
+    //   tl.to("#loadingAnimation", {
+    //     height: 0,
+    //     duration: 4,
 
-        ease: Expo.easeInOut,
-        delay: 1,
-        onComplete: () => {
-          setIsLoaded(true);
-        },
-      }).to("#childrens", {
-        height: "100vh",
-        delay: 1,
-        duration: 3,
-        transition: "Expo.ease",
-      });
-    }
-    return loadingFn();
+    //     ease: Expo.easeInOut,
+    //     delay: 1,
+    //     onComplete: () => {
+    //       setIsLoaded(true);
+    //     },
+    //   }).to("#childrens", {
+    //     height: "100vh",
+    //     delay: 1,
+    //     duration: 3,
+    //     transition: "Expo.ease",
+    //   });
+    // }
+    // return loadingFn();
+    gsap.to(".red", {
+      height: "0px",
+      duration: 3,
+      delay: 4,
+    });
+    gsap.to(".green", {
+      height: "100%",
+      duration: 3,
+      delay: 4,
+      onComplete: () => {
+        setIsLoaded(true);
+      },
+    });
   }, [{ isLoaded }]);
 
   return (
-    <div id="main" style={{ position: "relative" }}>
+    // <div id="main" style={{ position: "relative" }}>
+    //   <div
+    //     id="loadingAnimation"
+    //     style={{
+    //       height: "100vh",
+    //       // position: "absolute",
+    //       width: "100%",
+    //       display: isLoaded ? "none" : "",
+    //       transition: "all 0.1s ease",
+    //       // overflow: "hidden",
+    //     }}
+    //   >
+    //     <Loading id="loading" />
+    //   </div>
+    //   <div
+    //     id="childrens"
+    //     style={{
+    //       position: "absolute",
+    //       bottom: "0  ",
+
+    //       width: "100%",
+    //       height: "0px",
+    //       overflowY: "hidden",
+    //     }}
+    //   >
+    //     <Nevbar />
+    //     <Header />
+    //   </div>
+    //   <div>
+    //     {isLoaded && (
+    //       <>
+    //         <Itsolution />
+
+    //         <Story />
+    //         <ResponsiveCard />
+    //         <Connect />
+    //         <Footer />
+    //       </>
+    //     )}
+    //   </div>
+    // </div>
+    <div
+      style={{ background: "inherit", position: "relative", height: "100vh" }}
+    >
       <div
-        id="loadingAnimation"
         style={{
+          // background: "red",
+          position: "absolute",
           height: "100vh",
-          // position: "absolute",
+          display: isLoaded && "none",
           width: "100%",
-          display: isLoaded ? "none" : "",
-          transition: "all 0.1s ease",
-          // overflow: "hidden",
         }}
+        className="red"
       >
         <Loading id="loading" />
       </div>
       <div
-        id="childrens"
         style={{
+          // background: "green",
           position: "absolute",
-          top: "100%",
-
-          width: "100%",
+          bottom: "0",
           height: "0px",
+          overflowY: "hidden",
+          width: "100%",
         }}
+        className="green"
       >
-        <Nevbar />
-        <Header />
-        {isLoaded && (
-          <>
-            <Itsolution />
-
-            <Story />
-            <ResponsiveCard />
-            <Connect />
-            <Footer />
-          </>
-        )}
+        <div>
+          <Navbar />
+          <Header />
+        </div>
       </div>
+      {isLoaded && (
+        <div
+          style={{
+            width: "100%  ",
+            height: "100%",
+            position: "absolute",
+            top: "100%",
+            transition: "all 0.8s ease",
+          }}
+        >
+          <Itsolution />
+
+          <Story />
+          <ResponsiveCard />
+          <Connect />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
